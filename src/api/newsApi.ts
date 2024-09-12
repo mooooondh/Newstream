6f04c2d6a072638bce11e4f61592ac32f2873076
mooooondh <mdh9514@gmail.com>
2024-09-12 14:50:08 +0900

    feat : 뉴스 데이터 api개발
    * 뉴스 데이터를 가져오는 함수 getNewsApi 개발.
    * getNewsApi의 props로 검색어를 넣을 경우 해당 검색어로 뉴스 검색 기능 개발.
    * 검색어가 없는 경우 "오늘의 주요 뉴스"를 검색. (검색어가 api의 필수값임)

---
+++ src/api/newsApi.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { INaverNewsDataResult } from "@/utils/interfaces";
+
+export const getNewsApi = async (keyword?: string) => {
+	const naverNewsResult: INaverNewsDataResult | string = await axios({
+		method: "get",
+		url:
+			process.env.NAVER_SEARCH_BASE_URL +
+			"/v1/search/news.json?query=" +
+			encodeURI(keyword ? keyword : "오늘의 주요 뉴스"), // 네이버 뉴스 검색 api는 무조건 검색어가 있어야 함
+		headers: {
+			"X-Naver-Client-Id": process.env.NAVER_CLIENT_ID,
+			"X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET,
+		},
+	})
+		.then((result) => result.data)
+		.catch((error) => error.code);
+
+	return naverNewsResult;
+};
