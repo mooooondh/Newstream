93b9523e346fc4b120f10fc66b5d57da9dac665e
mooooondh <mdh9514@gmail.com>
2024-09-13 18:08:50 +0900

    feat : 검색 결과 화면 개발
    * 검색 결과화면 요소(뉴스 갯수, 뉴스 리스트 뉴스별 정보) 개발.
    * 뉴스 클릭시 외부 링크로 연결 기능 구현 필요.
    
    * test coverage 88.95%

--- src/app/search/[keyword]/page.tsx
+++ src/app/search/[keyword]/page.tsx
@@ -1,11 +1,39 @@
-export default function SearchResult({
+import dayjs from "dayjs";
+
+import { Search } from "@/components/Search";
+
+import { getNewsApi } from "@/api/newsApi";
+
+import { Btag2BoldText, decodeHtmlEntities } from "@/utils/stringTransformers";
+
+export default async function SearchResult({
   params,
 }: {
   params: { keyword: string };
 }) {
+  const newsData = await getNewsApi(params.keyword);
+
+  if (typeof newsData === "string") {
+    return (
+      <main>
+        <h1>데이터를 가져오던 중 오류가 발생했습니다.</h1>
+      </main>
+    );
+  }
+
   return (
-    <>
-      <h1>{params.keyword}</h1>
-    </>
+    <main>
+      <Search />
+      <h1>{"총 " + newsData.total + "개의 뉴스가 있습니다."}</h1>
+      <ul>
+        {newsData.items.map((news) => (
+          <li key={news.originallink}>
+            <h1>{Btag2BoldText(decodeHtmlEntities(news.title))}</h1>
+            <p>{dayjs(news.pubDate).format("YYYY.MM.DD HH:mm")}</p>
+            <h2>{Btag2BoldText(decodeHtmlEntities(news.description))}</h2>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
---
+++ src/utils/stringTransformers.tsx
@@ -0,0 +1,30 @@
+/** <b>태그로 감싸진 string을 bold텍스트로 변환
+ * 정규식으로 <b>태그를 찾은 뒤 해당 문자열을 실제 <b>태그로 감쌈
+ * <b>태그로 감싸지지 않은 문자는 그대로 둠
+ * 이 둘을 합쳐서 return
+ */
+export const Btag2BoldText = (text: string) => {
+  const parseBoldText = (text: string): (JSX.Element | string)[] => {
+    const parts = text.split(/(<b>.*?<\/b>)/gi);
+
+    return parts.map((part, index) => {
+      if (part.startsWith("<b>") && part.endsWith("</b>")) {
+        return <b key={index}>{part.substring(3, part.length - 4)} </b>;
+      }
+      return part;
+    });
+  };
+
+  return <p>{parseBoldText(text)}</p>;
+};
+
+/** html엔티티 디코드 */
+export const decodeHtmlEntities = (text: string) => {
+  return text
+    .replace(/&amp;/g, "&")
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'")
+    .replace(/&#96;/g, "`");
+};
