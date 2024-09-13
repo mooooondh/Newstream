827374a61009a9848599a9081ebd25e3b322f1ef
mooooondh <mdh9514@gmail.com>
2024-09-13 15:07:58 +0900

    style : 레이아웃 및 버튼 스타일링
    * 최대 가로 길이를 768px로 고정
    * 검색어 입력 및 검색 버튼 스타일링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Search } from "@/components/Search";
 
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <>
+    <main className={styles.wrapper}>
       <Search />
-    </>
+    </main>
   );
 }
